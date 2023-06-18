a0076cb058e8e31c3317575fc56f409585ae479b
Takura <takutadi1@gmail.com>
2023-06-18 14:51:12 +0200

    routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/HomePage";
+import LoginPage from "scenes/LoginPage";
+import ProfilePage from "scenes/ProfilePage";
+
 
 function App() {
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
